aba1a9ca089291d6062f1e4ed0a453330c4cd7a7
Sonawane Yash Prashant <YASHSONAWANE129910@GMAIL.COM>
2025-03-10 19:39:54 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5000/api/notes/";
+// const API_URL = "http://localhost:5000/api/notes/";
 const API_URL = "https://notes-app-syxd.onrender.com";
 // const API_URL = "https://notes-app-jorq.onrender.com";
 
